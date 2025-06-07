0606200ea36216c8e548f7eae1c02cb2d2374289
kankan ray <kankanray059@gmail.com>
2025-06-08 01:29:28 +0530

    jist coding club

---
+++ marquee.js
@@ -0,0 +1,74 @@
+function setupMarqueeAnimation() {
+    const marqueeItems = gsap.utils.toArray(".marquee h1");
+    if (marqueeItems.length > 0) {
+        const t1 = horizontalMarquee(marqueeItems, {
+            repeat: -1,
+            paddingRight: 30,
+
+        });
+        
+    }
+    
+}
+
+function horizontalLoop (items,config){
+    items = gsap.utils.toArray(items);
+    config = config || {};
+    let tl = gsap.timeline({
+        repeat: config.repeat,
+        defaults: {ease: "none"},
+    });
+    let length = items.length;
+    let startX = items[0].offsetLeft;
+    let widths= [];
+    let xPercents = [];
+    let pixelPerSecond = (config.speed || 1) / 100; 
+    let totalWidth , curX , distanceToStart , distanceToLoop, item, i;
+
+
+    gsap.set(items, {
+        xPercent: (i,el) => {
+            let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+            xPercents[i] = (parseFloat(gsap.getProperty(el,"x" , "px")) / w) * 100 +
+            gsap.getProperty(el, "xPercent");
+            return xPercents[i];
+        }      
+
+
+
+    });
+
+    gsap.set(items, { x:0});
+    totalWidth = items[length -1].offsetLeft + (xPercents[length -1] / 100) * widths[length -1] - startX +
+    items[length -1].offsetWidth* gsap.getProperty(items[length -1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+
+    for (i = 0; i < length; i++) {
+        item = items[i];
+        curX = (xPercents[i] / 100) * widths[i];
+        distanceToStart= item.offsetLeft + curX - startX;
+        distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(items, "scaleX");
+
+        tl.to(
+            item,
+            {
+                xPercent: ((curX+ distanceToLoop) / widths[i]) * 100,
+                duration: totalWidth / pixelPerSecond,
+            },
+            0
+        ).fromTo(
+            item,
+            { xPercent: ((curX - distanceToLoop + totalWidth) / widths[i]) * 100 },
+            { xPercent: xPercents[i],
+              duration: (curX - distanceToLoop + totalWidth- curX) / pixelPerSecond,
+              immediateRender: false,
+            },
+            distanceToLoop / pixelPerSecond
+        );
+        
+    }
+
+    tl.progress(1, true).progress(0, true);
+    return tl;
+
+
+}
